1933f6645c843a871a055e8999710e83b7b79856
Jakub Wyczesany <ghaelios@gmail.com>
2018-03-02 12:35:42 +0100

    Fetching categories through jservice

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    componentDidMount() {
+        fetch('http://jservice.io/api/categories?count=20')
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
+
     render() {
         return (
             <div>
