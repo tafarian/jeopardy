380d7d2f1d3da668de63eaf2aaba7c16da64072e
Jakub Wyczesany <ghaelios@gmail.com>
2018-03-02 12:29:12 +0100

    setting up jeopardy

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Jeopardy</h2>
+            </div>
+        )
+    }
+}
+
+export default App;
