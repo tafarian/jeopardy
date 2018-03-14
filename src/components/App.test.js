367e850b1200fbef842ebe77c0ff6829a08d76b5
Jakub Wyczesany <ghaelios@gmail.com>
2018-03-14 14:08:37 +0100

    testing the jeopardy app component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -10,6 +10,15 @@ describe('App', () => {
     const app = shallow(<App {...props} />);
 
     it('renders the title', () => {
-        expect(app.find('h2').text()).toEqual('Jeopardy!');
+        expect(app.find('h2').text()).toEqual('Jeopardy');
+    });
+
+    it('creates the correct number of links', () => {
+        expect(app.find('Link').length).toEqual(categories.length);
+    });
+    it('title the links correctly', () => {
+        app.find('Link h4').forEach((linkTitle, index) => {
+            expect(linkTitle.text()).toEqual(categories[index].title);
+        });
     });
 });
